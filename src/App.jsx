fcc5019736e126ae26f0a7fedfd66909c1a12a60
adityawintech <aditya#19AI>
2024-12-10 13:54:25 +0530

    Stagging

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes } from "react-router-dom"
+
+
+
+const App=()=>{
+   return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      
+    </Routes>
+    </BrowserRouter>
+    
+    </>
+   )
+
+}
+export default App
---
+++ src/Pages/Homepage/ndex.jsx
@@ -0,0 +1,12 @@
+
+
+
+const HomePage=()=>{
+    return (
+        <>
+         <h1>HomePage</h1>
+        
+        </>
+    )
+}
+export default HomePage
