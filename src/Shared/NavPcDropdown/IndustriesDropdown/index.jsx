761bdf570f0d00fecb849d12cf090bbace91b4fa
adityawintech <aditya#19AI>
2024-12-16 15:56:59 +0530

    okk

--- src/Shared/NavPcDropdown/IndustriesDropdown/index.jsx
+++ src/Shared/NavPcDropdown/IndustriesDropdown/index.jsx
@@ -1,18 +1,27 @@
+import { Link } from "react-router-dom";
+
 const IndustriesDropdown = () => {
     return (
         <>
-        <ul className="text-blue-900 z-40 p-5">
-      <li className="hover:text-blue-600 cursor-pointer py-1">
-        <a href="#construction">Construction</a>
-      </li>
-      <li className="hover:text-blue-600 cursor-pointer py-1">
-        <a href="#engineering">Engineering</a>
-      </li>
-      <li className="hover:text-blue-600 cursor-pointer py-1">
-        <a href="#architecture">Architecture</a>
-      </li>
-    </ul>
+            <ul className="text-blue-900 z-40 p-2">
+                <li className="hover:text-blue-600 cursor-pointer py-1">
+                    <Link to="/smart-infrastructure">SMART INFRASTRUCTURE (HOUSING)</Link>
+                </li>
+                <li className="hover:text-blue-600 cursor-pointer py-1">
+                    <Link to="/climate-change">CLIMATE CHANGE</Link>
+                </li>
+                <li className="hover:text-blue-600 cursor-pointer py-1">
+                    <Link to="/water-management">WATER MANAGEMENT</Link>
+                </li>
+                <li className="hover:text-blue-600 cursor-pointer py-1">
+                    <Link to="/mobility">MOBILITY (TRANSPORTATION)</Link>
+                </li>
+                <li className="hover:text-blue-600 cursor-pointer py-1">
+                    <Link to="/smart-industry">SMART INDUSTRY (INDUSTRY)</Link>
+                </li>
+            </ul>
         </>
-    )
-}
-export default IndustriesDropdown
+    );
+};
+
+export default IndustriesDropdown;
